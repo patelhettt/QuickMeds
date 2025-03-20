cf5460812faa576c494ee1230101b5685564bc66
patelhettt <162837495+patelhettt@users.noreply.github.com>
2025-03-20 07:03:09 +0530

    Corrected cancel button

--- frontend/src/components/buttons/CancelButton.js
+++ frontend/src/components/buttons/CancelButton.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { FcCancel } from 'react-icons/fc';
 
-const CancelButton = ({ extraClass }) => {
+const CancelButton = ({ extraClass, onClick }) => {
     return (
-        <button className={`btn btn-xs flex gap-x-2 ${extraClass}`}>
+        <button type="button" className={`btn btn-xs flex gap-x-2 ${extraClass}`} onClick={onClick}>
             <FcCancel className='text-lg' />
             Cancel
         </button>
